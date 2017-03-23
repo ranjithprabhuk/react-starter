510be30791cb56a2fa57e8bf5b06d2ab643a3a1c
ranjithprabhuk <ranjithprabhu.k@gmail.com>
2017-03-23 17:08:01 +0530

    sample app setup done

---
+++ src/app/app.component.jsx
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+import React from 'react';
+import Header from './header/header.component.jsx';
+
+class ReactApp extends React.Component {
+    render(){
+        return(
+            <Header />
+        );
+    }
+};
+
+export default ReactApp;
---
+++ src/app/header/header.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <nav className="navbar navbar-toggleable-md navbar-light bg-faded" >
+                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <a className="navbar-brand" href="#">React Starter</a>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Features</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Pricing</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav >
+        )
+    }
+};
+
+export default Header;
